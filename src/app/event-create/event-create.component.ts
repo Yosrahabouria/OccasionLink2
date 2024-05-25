84110fd2b3a7224c84b104a45e4f4c7e5c72898e
Yosrahabouria <yosrahabouria@gmail.com>
2024-05-25 19:00:32 +0200

    project

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -1,38 +1,39 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.component.html',
   styleUrls: ['./event-create.component.css']
 })
-export class EventCreateComponent {
-  model: any = {
-    eventType: '',
-    numberOfPeople: 0,
-    location: '',
-    date: ''
-  };
-
-  selectedEvent!: string;
-  numberOfPeople!: number;
-  selectedLocation!: string;
-  selectedDate: Date= new Date();
+export class EventCreateComponent implements OnInit {
+  eventForm: FormGroup;
   events = [{value: 'option1', label: 'Professional Meeting'}, {value: 'option2', label: 'Sex Gender Reveal'}, {value: 'option3', label: 'Anniversary'}];
   locations = [{value: '1', label: 'Tunis'}, {value: '2', label: 'La Marsa'}, {value: '3', label: 'Sidi Bousaid'}];
-  
-  constructor() {}
 
-  onSubmit(eventForm: any) {
-    if (eventForm.valid) {
-      const eventType = eventForm.value.eventType;
-      const numberOfPeople = eventForm.value.numberOfPeople;
-      const location = eventForm.value.location;
+  constructor() {
+    this.eventForm = new FormGroup({
+      eventType: new FormControl(''),
+      numberOfPeople: new FormControl(''),
+      location: new FormControl(''),
+      selectedDate: new FormControl('')
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+      const eventType = this.eventForm.value.eventType;
+      const numberOfPeople = this.eventForm.value.numberOfPeople;
+      const location = this.eventForm.value.location;
+      const selectedDate = this.eventForm.value.selectedDate;
 
       alert(`Event Created Successfully!\n` +
         `Event Type: ${eventType}\n` +
         `Number of People: ${numberOfPeople}\n` +
-        `Location: ${location}`);
+        `Location: ${location}\n` +
+        `Date: ${selectedDate}`);
     } else {
       alert('Please fill out all required fields.');
     }
